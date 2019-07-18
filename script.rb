d10712a5cc5072f8161180e4ab4ec2d745e15a57
Daniel Hongel and Tae Lee <daniel@>
2019-07-18 10:28:46 -0700

    it is refactoring without class definition

--- script.rb
+++ script.rb
@@ -4,5 +4,5 @@ require './lib/coin_logic.rb'
 p "Please enter a number of cents: $0."
 
 money = gets.chomp.to_i
-change = Coin.new(money)
-puts change.converter()
+# change = Coin.new(money)
+puts converter(money)
--- spec/coin_logic_spec.rb
+++ spec/coin_logic_spec.rb
@@ -4,47 +4,36 @@ require 'pry'
 
 describe('Coin') do
   it('gives one penny for one cent') do
-    change = Coin.new(1)
-    expect(change.converter()).to(eq("1 pennies"))
+    expect(converter(1)).to(eq("1 pennies"))
   end
   it('gives one nickels for five cents') do
-    change = Coin.new(5)
-    expect(change.converter()).to(eq("1 nickels"))
+    expect(converter(5)).to(eq("1 nickels"))
   end
   it('gives one nickels and one pennies for six cents') do
-    change = Coin.new(6)
-    expect(change.converter()).to(eq("1 nickels 1 pennies"))
+    expect(converter(6)).to(eq("1 nickels 1 pennies"))
   end
   it('gives one dime for ten cents') do
-    change = Coin.new(10)
-    expect(change.converter()).to(eq("1 dimes"))
+    expect(converter(10)).to(eq("1 dimes"))
   end
   it('gives one dime for 11 cents') do
-    change = Coin.new(11)
-    expect(change.converter()).to(eq("1 dimes 1 pennies"))
+    expect(converter(11)).to(eq("1 dimes 1 pennies"))
   end
   it('gives one dime for 15 cents') do
-    change = Coin.new(15)
-    expect(change.converter()).to(eq("1 dimes 1 nickels"))
+    expect(converter(15)).to(eq("1 dimes 1 nickels"))
   end
   it('gives one dime one nickels one pennies for 16 cents') do
-    change = Coin.new(16)
-    expect(change.converter()).to(eq("1 dimes 1 nickels 1 pennies"))
+    expect(converter(16)).to(eq("1 dimes 1 nickels 1 pennies"))
   end
   it('gives two dimes for 20 cents') do
-    change = Coin.new(20)
-    expect(change.converter()).to(eq("2 dimes"))
+    expect(converter(20)).to(eq("2 dimes"))
   end
   it('gives two dimes one pennies for 21 cents') do
-    change = Coin.new(21)
-    expect(change.converter()).to(eq("2 dimes 1 pennies"))
+    expect(converter(21)).to(eq("2 dimes 1 pennies"))
   end
   it('gives one quarters for 25 cents') do
-    change = Coin.new(25)
-    expect(change.converter()).to(eq("1 quarters"))
+    expect(converter(25)).to(eq("1 quarters"))
   end
   it('gives one quarters one dime one nickel one penny for 41 cents') do
-    change = Coin.new(41)
-    expect(change.converter()).to(eq("1 quarters 1 dimes 1 nickels 1 pennies"))
+    expect(converter(41)).to(eq("1 quarters 1 dimes 1 nickels 1 pennies"))
   end
 end
